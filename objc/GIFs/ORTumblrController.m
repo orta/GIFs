a581164e02722c67d72b5c1558bfc08e039a4624
Orta Therox <orta.therox@gmail.com>
2014-08-02 19:10:01 -0400

    tumblr de dupe and show best images support

--- objc/GIFs/ORTumblrController.m
+++ objc/GIFs/ORTumblrController.m
@@ -12,12 +12,80 @@
 #import "GIF.h"
 
 @interface TumblrGIF : GIF
-
 @property (copy, nonatomic, readwrite) NSString *representedURL;
-@property (assign, nonatomic, readwrite) NSInteger size;
+@property (copy, nonatomic, readwrite) NSString *tumblrID;
+
+@property (assign, nonatomic, readwrite) NSInteger suffixIndex;
+@property (assign, nonatomic, readwrite) BOOL isTumblr;
 @end
 
-@implementation TumblrGIF @end
+@implementation TumblrGIF
+
++ (NSArray *)suffixes
+{
+    return @[@"75sq", @"100", @"250", @"300", @"400", @"500"];
+}
+
+- (void)setRepresentedURL:(NSString *)representedURL
+{
+    self.isTumblr = NO;
+    
+    for (NSString *size in self.class.suffixes ) {
+        NSString *remove = [NSString stringWithFormat:@"_%@.gif", size];
+        
+        if ([representedURL containsString:remove]) {
+            self.suffixIndex = [self.class.suffixes indexOfObject:size];
+            self.isTumblr = YES;
+        }
+        representedURL = [representedURL stringByReplacingOccurrencesOfString:remove withString:@""];
+    }
+    
+    if (self.isTumblr) {
+        self.tumblrID = [[representedURL componentsSeparatedByString:@"tumblr.com"] lastObject];
+    }
+    _representedURL = representedURL;
+}
+
+- (id) imageRepresentation
+{
+    return self.downloadURL;
+}
+
+- (NSURL *)downloadURL
+{
+    if (!self.representedURL) return nil;
+    
+    if (self.isTumblr) {
+        NSString *fullURL = [NSString stringWithFormat:@"%@_%@.gif",self.representedURL, self.class.suffixes[self.suffixIndex]];
+        return  [NSURL URLWithString:fullURL];
+    } else {
+        return  [super downloadURL];
+    }
+}
+
+- (NSUInteger)hash
+{
+    if (self.isTumblr) {
+        return self.tumblrID.hash;
+    } else {
+        return self.representedURL.hash;
+    }
+}
+
+- (BOOL)isEqual:(id)object
+{
+    if ([object isKindOfClass:self.class]) {
+        if (self.isTumblr) {
+            return [self.tumblrID isEqual:[object tumblrID]];
+        } else {
+            return [self.representedURL isEqual:[object representedURL]];
+        }
+
+    }
+    return  [super isEqual:object];
+}
+
+@end
 
 @implementation ORTumblrController {
     NSString *_url;
@@ -63,8 +131,23 @@
 
                 if ([url.absoluteString rangeOfString:@".gif"].location != NSNotFound) {
                     NSString *address = [url.absoluteString stringByReplacingOccurrencesOfString:@"%5C" withString:@""];
-                    GIF *gif = [[GIF alloc] initWithDownloadURL:address thumbnail:address source:_url sourceTitle:_url ];
-                    if (gif) { [newGIFs addObject:gif]; }
+                    TumblrGIF *gif = [[TumblrGIF alloc] initWithDownloadURL:address thumbnail:address source:_url sourceTitle:_url ];
+                    gif.representedURL = address;
+                    if (gif) {
+                        if ([newGIFs containsObject:gif]) {
+
+                            // Set the suffix index to be the highest of the two
+                            
+                            for (TumblrGIF *searchGIF in newGIFs) {
+                                if ([searchGIF.representedURL isEqualToString:gif.representedURL]) {
+                                    searchGIF.suffixIndex = MAX(searchGIF.suffixIndex, gif.suffixIndex);
+                                }
+                            }
+                        } else {
+                            [newGIFs addObject:gif];
+                        }
+
+                    }
                 }
             }
         }];
