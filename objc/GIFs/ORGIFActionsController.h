0d7b4d3b8d5b10abe6934f8e27234fd5c8ea616e
Bryan Luby <bluby74@gmail.com>
2015-05-25 14:39:18 -0500

    Add helper class for handling contextual actions on GIFs

---
+++ objc/GIFs/ORGIFActionsController.h
@@ -0,0 +1,26 @@
+//
+//  ORGIFActionsController.h
+//  GIFs
+//
+//  Created by Bryan Luby on 5/25/15.
+//  Copyright (c) 2015 Orta Therox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  Helper class for shared contextual menu and keyboard shorcut actions on GIFs.
+ */
+@interface ORGIFActionsController : NSObject
+
++ (void)copyGIFDownloadURLToClipboard:(NSURL *)downloadURL;
+
++ (void)copyGIFMarkdownToClipboardWithSourceTitle:(NSString *)sourceTitle downloadURL:(NSURL *)downloadURL;
+
++ (void)openGIFDownloadURLInBrowser:(NSURL *)downloadURL;
+
++ (void)openGIFContextURLInBrowser:(NSURL *)contextURL;
+
++ (void)downloadGIFWithURL:(NSURL *)downloadURL completion:(void(^)(BOOL success, NSError *error))completion;
+
+@end
---
+++ objc/GIFs/ORGIFActionsController.m
@@ -0,0 +1,97 @@
+//
+//  ORGIFActionsController.m
+//  GIFs
+//
+//  Created by Bryan Luby on 5/25/15.
+//  Copyright (c) 2015 Orta Therox. All rights reserved.
+//
+
+#import "ORGIFActionsController.h"
+#import <AFNetworking/AFHTTPRequestOperation.h>
+
+@implementation ORGIFActionsController
+
++ (void)copyGIFDownloadURLToClipboard:(NSURL *)downloadURL
+{
+    [[NSPasteboard generalPasteboard] clearContents];
+    [[NSPasteboard generalPasteboard] writeObjects:@[downloadURL.absoluteString]];
+}
+
++ (void)copyGIFMarkdownToClipboardWithSourceTitle:(NSString *)sourceTitle downloadURL:(NSURL *)downloadURL
+{
+    [[NSPasteboard generalPasteboard] clearContents];
+    NSString *markdown = [NSString stringWithFormat:@"![%@](%@)", sourceTitle, downloadURL];
+    [[NSPasteboard generalPasteboard] writeObjects:@[markdown]];
+}
+
++ (void)openGIFDownloadURLInBrowser:(NSURL *)downloadURL
+{
+    [self openURL:downloadURL];
+}
+
++ (void)openGIFContextURLInBrowser:(NSURL *)contextURL
+{
+    [self openURL:contextURL];
+}
+
++ (void)openURL:(NSURL *)URL
+{
+    [[NSWorkspace sharedWorkspace] openURL:URL];
+}
+
++ (void)downloadGIFWithURL:(NSURL *)downloadURL completion:(void(^)(BOOL success, NSError *error))completion
+{
+    NSSavePanel *savePanel = [NSSavePanel savePanel];
+    savePanel.allowedFileTypes = @[@"gif"];
+    savePanel.canCreateDirectories = YES;
+    savePanel.allowsOtherFileTypes = NO;
+    savePanel.canSelectHiddenExtension = YES;
+    
+    NSString *downloadDirectoryPath = [ NSSearchPathForDirectoriesInDomains(NSDownloadsDirectory, NSUserDomainMask, YES) firstObject];
+    NSURL *downloadDirectoryURL = [NSURL fileURLWithPath:downloadDirectoryPath];
+    static NSURL * lastUserSelectedDirectoryURL = nil;
+    savePanel.directoryURL = lastUserSelectedDirectoryURL ? lastUserSelectedDirectoryURL : downloadDirectoryURL;
+    savePanel.nameFieldStringValue = downloadURL.lastPathComponent;
+    
+    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:downloadURL];
+    AFHTTPRequestOperation *afhttpRequestOperation = [[AFHTTPRequestOperation alloc] initWithRequest:urlRequest];
+    [afhttpRequestOperation
+     setDownloadProgressBlock:^(NSUInteger bytesRead, long long int totalBytesRead, long long int totalBytesExpectedToRead) {
+         
+     }];
+    
+    [savePanel beginWithCompletionHandler:^(NSInteger result) {
+        if (result) {
+            lastUserSelectedDirectoryURL = savePanel.directoryURL;
+            
+            [afhttpRequestOperation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, NSData *responseObject) {
+                BOOL success = [responseObject writeToURL:savePanel.URL atomically:YES];
+
+                NSError *error;
+                if (!success) {
+                    NSString *errorString = NSLocalizedString(@"Failed to save file", @"Failed to save file");
+                    error = [NSError errorWithDomain:NSCocoaErrorDomain
+                                                code:NSFileWriteUnknownError
+                                            userInfo:@{NSLocalizedDescriptionKey: errorString}];
+                    NSLog(@"%@", errorString);
+                } else {
+                    NSLog(@"Successfully downloaded file.");
+                }
+                
+                if (completion) {
+                    completion(success, error);
+                }
+            } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                NSLog(@"Fetch failed\n %@ \n %@", operation, error);
+                
+                if (completion) {
+                    completion(NO, error);
+                }
+            }];
+
+            [afhttpRequestOperation start];
+        }
+    }];
+}
+
+@end
