51ead5754612fe6cb1bbb8dfe26b106982c06d88
Orta Therox <orta.therox@gmail.com>
2015-10-22 07:25:42 +0100

    maybe fix swiftc issue

---
+++ objc/GIFs/ORThing.swift
@@ -0,0 +1,13 @@
+//
+//  ORThing.swift
+//  GIFs
+//
+//  Created by Orta on 22/10/2015.
+//  Copyright © 2015 Orta Therox. All rights reserved.
+//
+
+import UIKit
+
+class ORThing: NSObject {
+
+}
